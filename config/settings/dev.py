53d462594482ca1ce743ef75bdf9a46f933c7ee5
Jesse Michelsen <jmichelsen4@gmail.com>
2019-06-23 17:26:14 -0600

    Add config app for django settings and deployment stuff

---
+++ config/settings/dev.py
@@ -0,0 +1,16 @@
+from config.settings.base import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': get_env_variable('PSQL_HOST'),
+        'NAME': get_env_variable('PSQL_NAME'),
+        'USER': get_env_variable('PSQL_USERNAME'),
+        'PASSWORD': get_env_variable('PSQL_PASSWORD'),
+    }
+}
+
---
+++ config/settings/prod.py
@@ -0,0 +1,24 @@
+import django_heroku
+import dj_database_url
+
+from config.settings.base import *
+
+DEBUG = get_env_variable('PROD_DEBUG', False)
+SECRET_KEY = get_env_variable('DJANGO_SECRET_KEY')
+ALLOWED_HOSTS = ['localhost', '.bisl.com', 'www.bisl.com', 'bisl.herokuapp.com']
+
+CSRF_COOKIE_SECURE = True
+X_FRAME_OPTIONS = 'DENY'
+SESSION_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = bool(get_env_variable('DJANGO_SSL_REDIRECT', default=False))
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_HOST = 'bisl.com'
+
+# Database
+# Heroku: Update database configuration from $DATABASE_URL.
+DATABASES = {'default': dj_database_url.config(conn_max_age=500)}
+
+django_heroku.settings(locals(), logging=False, staticfiles=False)
+
