bd920eb8f97c8166ce5cf219e301a8f2833daa47
Jonathan Mast <jonathanemast@gmail.com>
2019-08-06 12:40:38 -0400

    Update comments app model

--- comments/models.py
+++ comments/models.py
@@ -1,17 +1,18 @@
+from django.conf import settings
 from django.db import models
 from common.mixins.model import TimestampMixin
-from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Comment(TimestampMixin, models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     recipe = models.ForeignKey('recipes.Recipe', on_delete=models.CASCADE)
-    text = models.TextField(max_length=2000)
+    text = models.TextField()
 
     class Meta:
         ordering = ['-created_at']
-        verbose_name_plural = 'comments'
 
     def __str__(self):
-        return f"{self.author}: {self.text[:50]}"
+        return f"{self.author}: {self.text[:50]}..."
