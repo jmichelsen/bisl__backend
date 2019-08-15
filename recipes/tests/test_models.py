d5facbaa347e06b5daadcf364974d1224a83f39f
Jonathan Mast <jonathanemast@gmail.com>
2019-08-15 18:10:47 -0400

    Break down recipe model testing

--- recipes/tests/test_models.py
+++ recipes/tests/test_models.py
@@ -5,10 +5,10 @@ from django.test import TestCase
 from recipes.models import Recipe
 
 
-class RecipeModelTest(TestCase):
+class RecipeTestCase(TestCase):
 
     def setUp(self):
-        self.obj1 = Recipe.objects.create(
+        Recipe.objects.create(
             title='test_title',
             ingredients='test_ingredients',
             preparation_process='test_prep',
@@ -16,27 +16,15 @@ class RecipeModelTest(TestCase):
             number_of_portions=2,
             difficulty=1)
 
-    def test_title(self):
-        self.assertEquals(self.obj1.title, 'test_title')
+    def test_string(self):
+        expected = 'test_title'
+        actual = Recipe.objects.get(title='test_title').__str__()
+        self.assertEquals(expected, actual)
 
-    def test_title_max_length(self):
-        max_length = self.obj1._meta.get_field('title').max_length
+     def test_title_max_length(self):
+        max_length = Recipe._meta.get_field('title').max_length
         self.assertEquals(max_length, 100)
 
-    def test_ingredients(self):
-        self.assertEquals(self.obj1.ingredients, 'test_ingredients')
-
-    def test_preparation_process(self):
-        self.assertEquals(self.obj1.preparation_process, 'test_prep')
-
-    def test_preparation_time(self):
-        self.assertEquals(self.obj1.preparation_time, timedelta(minutes=20))
-
-    def test_number_of_portions(self):
-        self.assertEquals(self.obj1.number_of_portions, 2)
-
-    def test_difficulty(self):
-        self.assertEquals(self.obj1.difficulty, 1)
-
-    def test_string(self):
-        self.assertEquals(self.obj1.__str__(), self.obj1.title)
+    def test_ingredients_max_length(self):
+        max_length = Recipe._meta.get_field('ingredients').max_length
+        self.assertEquals(max_length, 2000)
