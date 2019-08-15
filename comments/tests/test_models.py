22bc1081db53e8d94733db0adbae91ea4ae24a7e
Jonathan Mast <jonathanemast@gmail.com>
2019-08-15 18:09:54 -0400

    Break down comment model testing

--- comments/tests/test_models.py
+++ comments/tests/test_models.py
@@ -10,39 +10,30 @@ from comments.models import Comment
 
 class CommentModel(TestCase):
 
-    def setUp(self):
+    @classmethod
+    def setUpTestData(cls):
 
         user = User.objects.create(username='test_user')
 
-        self.obj1 = Recipe.objects.create(
+        test_recipe = Recipe.objects.create(
             title='test_title',
             ingredients='test_ingredients',
             preparation_process='test_prep',
             preparation_time=timedelta(minutes=20),
             number_of_portions=2,
-            difficulty=1
-        )
+            difficulty=1)
 
-        self.obj2 = Comment.objects.create(
+        Comment.objects.create(
             author=user,
-            recipe=self.obj1,
-            text='testing'
-        )
-
-    def test_author(self):
-        self.assertEquals(self.obj2.author.username, 'test_user')
-
-    def test_recipe(self):
-        self.assertEquals(self.obj2.recipe.title, 'test_title')
-
-    def test_text(self):
-        self.assertEquals(self.obj2.text, 'testing')
-
-    def test_ordering(self):
-        ordering = self.obj2._meta.ordering
-        self.assertEquals(ordering[0], '-created_at')
+            recipe=test_recipe,
+            text='testing')
 
     def test_string(self):
-        self.assertEquals(self.obj2.__str__(), 'test_user: testing...')
+        expected = 'test_user: testing...'
+        actual = Comment.objects.get(id=1).__str__()
+        self.assertEquals(expected, actual)
 
-
+    def test_string_fail(self):
+        expected = 'test_user: testing'
+        actual = Comment.objects.get(id=1).__str__()
+        self.assertFalse(expected, actual)
