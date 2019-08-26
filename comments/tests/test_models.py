061a2e8937c7ae9a8b03282cd119f00ebc1402ed
Jonathan Mast <jonathanemast@gmail.com>
2019-08-26 16:54:03 -0400

    Reference get_user_model() Group imports alphabetically

--- comments/tests/test_models.py
+++ comments/tests/test_models.py
@@ -1,16 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.contrib.auth.models import User
 
 from fixtureless.factory import create
 
-from recipes.models import Recipe
 from comments.models import Comment
+from recipes.models import Recipe
 
 
 class TestCommentModel(TestCase):
 
     def setUp(self):
-        self.user = User.objects.create(username='test_user')
+        self.user = get_user_model().objects.create(username='test_user')
         self.comment = create(Comment)
         self.recipe = create(Recipe)
 
