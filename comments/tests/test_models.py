8fffabbd1dd24ee778214dbe1bde12355776e331
Jonathan Mast <jonathanemast@gmail.com>
2019-08-26 15:04:10 -0400

    Add fixtureless test for Comment model

--- comments/tests/test_models.py
+++ comments/tests/test_models.py
@@ -1,39 +1,21 @@
-from datetime import timedelta
-
 from django.test import TestCase
 from django.contrib.auth.models import User
 
+from fixtureless.factory import create
 
 from recipes.models import Recipe
 from comments.models import Comment
 
 
-class CommentModel(TestCase):
+class TestCommentModel(TestCase):
 
-    @classmethod
-    def setUpTestData(cls):
+    def setUp(self):
+        self.user = User.objects.create(username='test_user')
+        self.comment = create(Comment)
+        self.recipe = create(Recipe)
 
-        user = User.objects.create(username='test_user')
+    def test_comment_str(self):
+        expected = Comment.__str__(self.comment)
+        actual = self.comment.__str__()
+        self.assertEqual(expected, actual)
 
-        test_recipe = Recipe.objects.create(
-            title='test_title',
-            ingredients='test_ingredients',
-            preparation_process='test_prep',
-            preparation_time=timedelta(minutes=20),
-            number_of_portions=2,
-            difficulty=1)
-
-        Comment.objects.create(
-            author=user,
-            recipe=test_recipe,
-            text='testing')
-
-    def test_string(self):
-        expected = 'test_user: testing...'
-        actual = Comment.objects.get(id=1).__str__()
-        self.assertEquals(expected, actual)
-
-    def test_string_fail(self):
-        expected = 'test_user: testing'
-        actual = Comment.objects.get(id=1).__str__()
-        self.assertFalse(expected, actual)
