50b2796cc2daba010d33c5e4c92407c08042a7da
Jonathan Mast <jonathanemast@gmail.com>
2019-10-16 16:33:52 -0400

    Allow users to search by exact Ingredient

--- recipes/filters.py
+++ recipes/filters.py
@@ -4,8 +4,11 @@ from recipes.models import Recipe
 
 
 class RecipeFilter(django_filters.FilterSet):
+    """
+    Allow users to search by EXACT ingredient
+    """
 
     class Meta:
         model = Recipe
-        fields = ['ingredients']
+        fields = ['ingredients__name']
 
