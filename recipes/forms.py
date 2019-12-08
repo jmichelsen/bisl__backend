2f231cdd58edb31cfe7032b7c868c5adcc297c96
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2019-12-08 20:32:20 +0100

    Add Ingredient and Step forms

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,10 +1,37 @@
 from django import forms
 
 # local
-from .models import Recipe
+from .models import Recipe, Ingredient, Step, Tip
 
 
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        exclude = ('user', 'users_who_made_this', 'ingredients', 'votes')
+        exclude = (
+            'user',
+            'users_who_made_this',
+            'ingredients',
+            'votes',
+        )
+
+
+class IngredientForm(forms.ModelForm):
+    class Meta:
+        model = Ingredient
+        fields = (
+            'name',
+            'description',
+            'quantity',
+            'weight',
+            'volume',
+        )
+
+
+class StepForm(forms.ModelForm):
+    class Meta:
+        model = Step
+        exclude = (
+            'recipe',
+            'number',
+            'completed'
+        )
--- recipes/tests/test_forms.py
+++ recipes/tests/test_forms.py
@@ -1,9 +1,13 @@
 from datetime import timedelta
 from django.test import TestCase
+from fixtureless.factory import create
+
+# third-party
+from measurement.measures import Volume, Weight
 
 # local
-from recipes.forms import RecipeForm
-from recipes.models import Recipe
+from recipes.forms import RecipeForm, IngredientForm, StepForm
+from recipes.models import Recipe, Ingredient, Step
 
 
 class TestRecipeFrom(TestCase):
@@ -35,7 +39,6 @@ class TestRecipeFrom(TestCase):
     def test_invalid_form(self):
         """
         Validate an invalid form
-        :return:
         """
         r = Recipe(
             title='',
@@ -58,3 +61,58 @@ class TestRecipeFrom(TestCase):
         form = RecipeForm(data=form_data)
         self.assertFalse(form.is_valid())
 
+
+class TestIngredientForm(TestCase):
+    def test_valid_form(self):
+        """
+        Validate a valid form
+        """
+        i = Ingredient.objects.create(
+            name='test_name',
+            description='test_description',
+            quantity=1.0,
+            weight=Weight(kg=1),
+            volume=Volume(us_pint=1),
+        )
+        form_data = {
+            'name': i.name,
+            'description': i.description,
+            'quantity': i.quantity,
+            'weight': i.weight,
+            'volume': i.volume,
+        }
+        form = IngredientForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form(self):
+        """
+        Validate an invalid form
+        """
+        form_data = {
+            'name': '',
+            'description': '',
+            'quantity': None,
+            'weight': None,
+            'volume': None,
+        }
+        form = IngredientForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+
+class TestStepForm(TestCase):
+    def test_valid_form(self):
+        """
+        Validate a valid form
+        """
+        s = create(Step)
+        form_data = {'name': s.name, 'directions': s.directions}
+        form = StepForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form(self):
+        """
+        Validate an invalid form
+        """
+        form_data = {'name': '', 'directions': ''}
+        form = StepForm(data=form_data)
+        self.assertFalse(form.is_valid())
