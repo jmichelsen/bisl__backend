984f41ebc90044bf6486ea2049b8ea6f9f0eb2c9
Jonathan Mast <jonathanemast@gmail.com>
2019-12-14 12:56:36 -0500

    Add recipe admin testing

--- recipes/tests/test_views.py
+++ recipes/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group, Permission
 from django.test import TestCase, Client
 from django.urls import reverse
 from fixtureless.factory import create
@@ -10,21 +11,33 @@ class TestRecipeViews(TestCase):
 
     def setUp(self):
         super().setUp()
-        self.user = get_user_model().objects.create(username='test_user')
-        self.form = create(Recipe, {'title': 'test_title'})
+        self.user1 = get_user_model().objects.create(username='test_user1')
+        self.form = create(Recipe, {'user': self.user1, 'title': 'test_title'})
+        self.client = Client()
+
+        # group and permission setup
+        self.group = Group(name='recipe admin')
+        self.group.save()
+        add_delete_permission = Permission.objects.get(name='Can delete recipe')
+        add_edit_permission = Permission.objects.get(name='Can change recipe')
+
+        self.user2 = get_user_model().objects.create(username='test_user', password='test_pw')
+        self.user2.groups.add(self.group)
+        self.group.permissions.add(add_delete_permission, add_edit_permission)
 
     def test_create_recipe(self):
         """
         Test form instance uses self.user for posting recipes
         """
-        self.client.post(reverse('recipes:create'), {'title': self.form.title})
+        self.client.force_login(user=self.user1)
+        response = self.client.post(reverse('recipes:create'), {'title': self.form.title})
         self.assertEqual(Recipe.objects.first().user, self.form.user)
+        self.assertContains(response, 'test_title')
 
     def test_logged_out_user(self):
         """
         Test logged out user can not create,  redirects user to login page
         """
-        self.client = Client()
         self.client.logout()
         response = self.client.get(reverse('recipes:create'))
         self.assertRedirects(response, '/accounts/login/?next=/recipe/create/')
@@ -37,7 +50,7 @@ class TestRecipeViews(TestCase):
         """
         Test UpdateView updates users recipe
         """
-        self.client.force_login(user=self.user)
+        self.client.force_login(user=self.user1)
         response = self.client.put(reverse('recipes:update', kwargs={'pk': self.form.pk}),
                                    {'title': 'testing'})
         self.assertEqual(response.status_code, 200)
@@ -58,7 +71,7 @@ class TestRecipeViews(TestCase):
         """
         Test DeleteView takes user to confirmation page
         """
-        self.client.force_login(user=self.user)
+        self.client.force_login(user=self.user1)
         response = self.client.get(reverse('recipes:delete', kwargs={'pk': self.form.pk}))
         self.assertContains(response, 'delete')
 
@@ -66,7 +79,7 @@ class TestRecipeViews(TestCase):
         """
         Test DeleteView deletes a recipe
         """
-        self.client.force_login(user=self.user)
+        self.client.force_login(user=self.user1)
         response = self.client.post(reverse('recipes:delete', kwargs={'pk': self.form.pk}))
         self.assertRedirects(response, reverse('recipes:list'))
         self.assertFalse(Recipe.objects.filter(pk=self.form.pk).exists())
@@ -78,3 +91,22 @@ class TestRecipeViews(TestCase):
         self.client.force_login(user=get_user_model().objects.create(username='unauthorized_user'))
         response = self.client.get(reverse('recipes:delete', kwargs={'pk': self.form.pk}))
         self.assertEqual(response.status_code, 403)
+
+    def test_admin_can_delete_recipe(self):
+        """
+        Test recipe admin can delete recipe
+        """
+        self.client.force_login(user=self.user2)
+
+        response = self.client.post(reverse('recipes:delete', kwargs={'pk': self.form.pk}))
+        self.assertRedirects(response, reverse('recipes:list'))
+
+    def test_admin_can_update_recipe(self):
+        """
+        Test recipe admin can update users recipe
+        """
+        self.client.force_login(user=self.user2)
+        response = self.client.put(reverse('recipes:update', kwargs={'pk': self.form.pk}),
+                                   {'title': 'testing123'})
+        self.assertEqual(response.status_code, 200)
+
