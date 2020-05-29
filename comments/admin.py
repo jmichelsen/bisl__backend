c1baea460955396c9a16571a0f6096a11f4aecee
Jonathan Mast <jonathanemast@gmail.com>
2020-05-29 14:18:20 -0400

    Add CommentAdmin

--- comments/admin.py
+++ comments/admin.py
@@ -4,6 +4,18 @@ from .models import Comment
 
 # Register your models here.
 
-admin.site.register(Comment)
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('username', 'recipe', 'text', 'created_at', 'active')
+    list_filter = ('active', 'created_at')
+    search_fields = ('author__username', 'recipe__title', 'text')
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
+
+    def username(self, obj):
+        return obj.author.username
+
+
+admin.site.register(Comment, CommentAdmin)
 
